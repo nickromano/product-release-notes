3e2afcb6389c262153c4aa7781166c44bbd48cc2
Nick Romano <nick.r.romano@gmail.com>
2017-08-16 19:39:44 -0700

    Include migrations in package

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ DESCRIPTION = "Release notes page for your product."
 
 setup(
     name="product_release_notes",
-    version="0.0.1",
+    version="0.0.2",
     url='https://github.com/nickromano/product-release-notes',
     license='BSD',
     description=DESCRIPTION,
     long_description=DESCRIPTION,
     author='Nick Romano',
     author_email='nick.r.romano@gmail.com',
-    packages=['product_release_notes'],  # , 'product_release_notes.migrations'],
+    packages=['product_release_notes', 'product_release_notes.migrations'],
     install_requires=[
         'Django>=1.8',
     ],
